5f979e56c353846973127a3981d1317b2f4903d7
mbulldozer <mbulldozer@mail.ru>
2018-09-17 00:50:04 +0300

    task closed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var qEquation = equation.replace(/\s+/g,'');
+    var posa = qEquation.indexOf("*x^2");
+    var posb = qEquation.indexOf("*x", posa+4);
+    var a = +qEquation.substring(0, posa);
+    var b = +qEquation.substring(posa + 4, posb);
+    var c = +qEquation.substring(posb + 2);
+    var discriminant = b*b - 4*a*c;
+    var x1, x2;
+    x1 = Math.round((-b + Math.sqrt(b*b - 4*a*c))/(2*a));
+    x2 = Math.round((-b - Math.sqrt(b*b - 4*a*c))/(2*a));
+    var rezalt = [x1, x2];
+    return rezalt.sort((a, b) => a - b);
 }
